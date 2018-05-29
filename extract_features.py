35b9484cc39cd0c36b59058ea2f9b49ccf02fd32
Nikola Ljubešić <nljubesi@gmail.com>
2018-05-29 11:30:16 +0000

    Relative directory path corrected

--- extract_features.py
+++ extract_features.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import os,sys
 reldir = os.path.dirname(os.path.abspath(__file__))
-file = os.path.join(reldir, sys.argv[1])
-patterns = os.path.join(reldir, sys.argv[2])
-idf = os.path.join(sys.argv[3])
+file = sys.argv[1]
+patterns = sys.argv[2]
+idf = sys.argv[3]
 for stat in ('chisq','dice','ll','mi','tscore','tfidf'):
     print '## statistic',stat
     for l in range(4):
@@ -11,8 +11,8 @@ for stat in ('chisq','dice','ll','mi','tscore','tfidf'):
             continue
         print '# length',l+1
         if stat == 'tfidf':
-            print 'python CollTerm.py -seq 2 -p ' + patterns + ' -i ' + file + ' -m ' + stat + ' -l ' + str(l + 1) + ' -pos 0 3 2 -idf ' + idf + ' -norm 1 > ' + file + '.' + stat + '.' + str(l + 1)
-            os.system('python CollTerm.py -seq 2 -p ' + patterns + ' -i ' + file + ' -m ' + stat + ' -l ' + str(l + 1) + ' -pos 0 3 2 -idf ' + idf + ' -norm 1 > ' + file + '.' + stat + '.' + str(l + 1))
+            print 'python ' + os.path.join(reldir, 'CollTerm.py') + ' -seq 2 -p ' + patterns + ' -i ' + file + ' -m ' + stat + ' -l ' + str(l + 1) + ' -pos 0 3 2 -idf ' + idf + ' -norm 1 > ' + file + '.' + stat + '.' + str(l + 1)
+            os.system('python ' + os.path.join(reldir, 'CollTerm.py') + ' -seq 2 -p ' + patterns + ' -i ' + file + ' -m ' + stat + ' -l ' + str(l + 1) + ' -pos 0 3 2 -idf ' + idf + ' -norm 1 > ' + file + '.' + stat + '.' + str(l + 1))
         else:
-            print 'python CollTerm.py -seq 2 -p ' + patterns + ' -i ' + file + ' -m ' + stat + ' -l ' + str(l + 1) + ' -pos 0 3 2 -norm 1 > ' + file + '.' + stat + '.' + str(l + 1)
-            os.system('python CollTerm.py -seq 2 -p ' + patterns + ' -i ' + file + ' -m ' + stat + ' -l ' + str(l + 1) + ' -pos 0 3 2 -norm 1 > ' + file + '.' + stat + '.' + str(l + 1))
+            print 'python ' + os.path.join(reldir, 'CollTerm.py') + ' -seq 2 -p ' + patterns + ' -i ' + file + ' -m ' + stat + ' -l ' + str(l + 1) + ' -pos 0 3 2 -norm 1 > ' + file + '.' + stat + '.' + str(l + 1)
+            os.system('python ' + os.path.join(reldir, 'CollTerm.py') + ' -seq 2 -p ' + patterns + ' -i ' + file + ' -m ' + stat + ' -l ' + str(l + 1) + ' -pos 0 3 2 -norm 1 > ' + file + '.' + stat + '.' + str(l + 1))
